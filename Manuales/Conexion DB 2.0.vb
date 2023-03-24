1ebf3f8b0ca245c38a591d069abf0ef6d50d8c2e
edujimser <119799007+edujimser@users.noreply.github.com>
2023-03-25 00:48:26 +0100

    Add files via upload

---
+++ Manuales/Conexion DB 2.0.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Data.SqlClient
+Imports Microsoft.SqlServer
+Imports Microsoft.VisualBasic.ApplicationServices
+
+Public Class conexion
+    Public conn As SqlConnection
+
+    Public Sub New()
+        conn = New SqlConnection("Data Source=WINDOWS10;Initial Catalog=dbsistema;Persist Security Info=True;User ID=sa;Password=1234")
+    End Sub
+End Class
